d48dc9f01ca0e4e59b891964ef774a767d52fdb5
blocage <marrytheuser@gmail.com>
2024-05-10 15:29:40 +0400

    Added deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+rm -rf build
+npm run build
+cd ./build
+git init
+git add .
+git commit -m 'push to gh-pages'
+git push --force git@github.com:blocage/leetcode-algorithms.git main:gh-pages
+cd ../
+git tag `date "+release-%Y%m%d%H%M%S"`
+git push --tags
